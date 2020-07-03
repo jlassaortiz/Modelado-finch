e794a4f7585fab9a21171ffc408a5b60af1f43b0
Javier Lassa Ortiz <javi_lassaortiz@MacBook-Pro-de-Javier.local>
2020-07-03 19:02:48 -0300

    Ordeno un poco y guardo salida de fuente como .wav

--- sintesis.py
+++ sintesis.py
@@ -143,7 +143,7 @@ gamma = 12300 #12500
 
 # Parametros tracto vocal
 uoch = 40*2700*2700 # 40*2700*2700
-uolg =  1./1. # 1./1.
+uolg =  30./1. # 1./1.
 rdis = 5000/1.0 # 5000/1.0
 
 v = np.zeros(5)
@@ -178,7 +178,7 @@ for i in range(np.int(tiempo_total/(dt))):
 # rectas(ti,tf,wi,wf,factor,frequencias,amplitudes)
 # senito(ti,tf,media,amplitud,alphai,alphaf,factor,frequencias, amplitudes)
 
-
+# Notas intro
 expo(0.021233,0.070797,869.3,475.2,0.2239/0.944,frequencias, amplitudes)
 expo(0.163317,0.205335,825.5,453,0.2714/0.944,frequencias, amplitudes)
 expo(0.271632,0.313879,847.4,475.1,0.3216/0.944,frequencias, amplitudes)
@@ -187,11 +187,16 @@ rectas(0.467733,0.479150,812.64,812.64,0.08789/0.944,frequencias, amplitudes)
 expo(0.479152,0.527799,897,518.9,0.325/0.944,frequencias,amplitudes)
 rectas(0.592287,0.631223,573,431,0.3074/0.944,frequencias, amplitudes)
 rectas(0.699323,0.726651,847.4,497,0.2258/0.944,frequencias, amplitudes)
+
 rectas(0.794424,0.824770,1550,1550,0.08599/0.944,frequencias, amplitudes)
+# C
 expo(0.824771,0.929312,900,518.9,0.7771/0.944,frequencias, amplitudes)
+
+# B
 rectas(0.956319,0.969585,475,781,0.3498/0.944,frequencias, amplitudes)
 rectas(0.969586,1.000902,871,871,0.4434/0.944,frequencias, amplitudes)
 
+# A
 #rectas(1.035779,1.036574,475,570,0.389/0.944,frequencias, amplitudes)
 #rectas(1.036578,1.099138,579.72,559.72,0.9205/0.944,frequencias, amplitudes)
 #rectas(1.099139,1.107158,579.72,409.4,0.2376/0.944,frequencias, amplitudes)
@@ -201,17 +206,23 @@ rectas(1.035681,1.108332,576,576,0.9205/0.944,frequencias, amplitudes)
 rectas(1.134231,1.139772,935,1548,0.1221/0.944,frequencias, amplitudes)
 rectas(1.141474,1.160209,546.62,546.62,0.1221/0.944,frequencias, amplitudes)
 rectas(1.160210,1.173081,546.62,344,0.4057/0.944,frequencias, amplitudes)
+
 senito(1.212321,1.223899,1132,1877,-np.pi/2,np.pi/2,0.2076/0.944,frequencias, amplitudes)
 senito(1.2240,1.251077,2534,963,-3*np.pi/2,15*np.pi/2,0.3622/0.944,frequencias, amplitudes)
 expo(1.259591,1.284083,1263,803,0.3622/0.944,frequencias, amplitudes)
+
 rectas(1.357033,1.364902,1571,1571,0.531/0.944,frequencias, amplitudes)
 rectas(1.366208,1.382211,1592,1592,0.8903/0.944,frequencias,amplitudes)
 rectas(1.386501,1.486461,1023,475,0.7487/0.944,frequencias,amplitudes)
 rectas(1.521486,1.529366,497,847,0.3285/0.944,frequencias,amplitudes)
 rectas(1.530969,1.564968,977,828,0.4556/0.944,frequencias,amplitudes)
-rectas(1.599836,1.606783,497,565,0.3814/0.944,frequencias,amplitudes)
-rectas(1.606784,1.660270,558,558,0.7228/0.944,frequencias,amplitudes)
-rectas(1.660270,1.670171,558,387.5,0.4041/0.944,frequencias,amplitudes)
+
+# A2
+#rectas(1.599836,1.606783,497,565,0.3814/0.944,frequencias,amplitudes)
+#rectas(1.606784,1.660270,558,558,0.7228/0.944,frequencias,amplitudes)
+#rectas(1.660270,1.670171,558,387.5,0.4041/0.944,frequencias,amplitudes)
+rectas(1.599836,1.670171,564,564.5,0.7228/0.944,frequencias,amplitudes)
+
 rectas(1.699486,1.726439,540.73,540.73,0.4691/0.944,frequencias,amplitudes)
 rectas(1.726440,1.737927,540.73,365.6,0.2306/0.944,frequencias,amplitudes)
 senito(1.775943,1.787108,1142,563,-np.pi,np.pi,0.1919/0.944,frequencias, amplitudes)
@@ -322,7 +333,11 @@ sonido = np.asarray(sonido)
 # Este paso es necesario para que el archivo wav se guarde correctamente
 # Ver la documentacion de: scipy.io.wavfile.write
 scaled = np.int16(sonido/np.max(np.abs(sonido)) * 32767)
-write('sintesis_finch_pol5.wav', 44100, scaled)
+write('sintesis_finch_uolg_30.wav', 44100, scaled)
+
+# Guardo salida de fuente.
+y_scaled = np.int16(y_out/np.max(np.abs(y_out)) * 32767)
+write('y.wav', 44100, y_scaled)
 
 
 
