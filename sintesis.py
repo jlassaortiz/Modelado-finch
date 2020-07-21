33003550757e0e21342035d9f96c17fd848f22ad
Javier Lassa Ortiz <javi_lassaortiz@MacBook-Pro-de-Javier.local>
2020-07-21 11:26:18 -0300

    GRAN CAMBIO. Parametros especificos del canto en archivo aparte

--- sintesis.py
+++ sintesis.py
@@ -15,7 +15,6 @@ Estructura del codigo:
 
 """
 
-
 import numpy as np     	
 from scipy.io.wavfile import write
 import random
@@ -28,13 +27,10 @@ global feedback1
 global estimulo1
 global destimulodt1
 
-
-
     
 # -----------------------
 # Definicion de funciones
 # -----------------------
-
 
 
 # Sistema de ecuaciones del modelo
@@ -94,14 +90,15 @@ def rk4(dv,v,n,t,dt): #  dv es la funcion ecuaciones()
 
 
 # Toma a la frec fundamental de cada silaba como una expo, recta o seno y
-# modifica alpha para que fone el sistema y devuelve frecuencias y amplitudes
+# modifica alpha para que fone el sistema y frecuencias y amplitudes
 def expo(ti,tf,wi,wf,factor,frequencias,amplitudes):
     i=np.int(ti/dt)
     j=np.int(tf/dt)
     for k in range((j-i)):
         t=ti+k*dt
         frequencias[i+k]=wf+(wi-wf)*np.exp(-3*(t-ti)/((tf-ti)))
-        alpha[i+k]=-0.150 # alpha suficiente para fonar 150
+        alpha[i+k]=-0.150 # alpha suficiente para fonar
+        #alpha[i+k]=-0.125
         #amplitudes[i+k]=factor*np.sin(np.pi*k/(j-i)) * (1+random.normalvariate(0.,.4)) +factor/10 * (1+random.normalvariate(0.,.02))
         amplitudes[i+k]=factor*np.sin(np.pi*k/(j-i)) * (1+random.normalvariate(0.,.2)) +factor/10 * (1+random.normalvariate(0.,.01))
     return frequencias,amplitudes
@@ -112,11 +109,12 @@ def rectas(ti,tf,wi,wf,factor,frequencias,amplitudes):
     j=np.int(tf/dt)
     for k in range((j-i)):
         t=ti+k*dt
-        frequencias[i+k]=wi+(wf-wi)*(t-ti)/(tf-ti) 
-        alpha[i+k]=-0.150 # alpha suficiente para fonar 150
+        frequencias[i+k]= wi + (wf-wi)*(t-ti)/(tf-ti) 
+        alpha[i+k]=-0.150 # alpha suficiente para fonar
+        #alpha[i+k]=-0.125
         #amplitudes[i+k]=factor*np.sin(np.pi*k/(j-i))*(1+random.normalvariate(0.,.4))
         amplitudes[i+k]=factor*np.sin(np.pi*k/(j-i))*(1+random.normalvariate(0.,.2))
-    return frequencias,amplitudes #150*
+    return frequencias,amplitudes
 
 
 def senito(ti,tf,media,amplitud,alphai,alphaf,factor,frequencias, amplitudes):
@@ -125,7 +123,8 @@ def senito(ti,tf,media,amplitud,alphai,alphaf,factor,frequencias, amplitudes):
     for k in range((j-i)):
         t = ti+k*dt
         frequencias[i+k]=media+amplitud*np.sin(alphai+(alphaf-alphai)*(t-ti)/(tf-ti))
-        alpha[i+k]=-0.150 # alpha suficiente para fonar 150
+        alpha[i+k]=-0.150 # alpha suficiente para fonar
+        #alpha[i+k]=-0.125  
         #amplitudes[i+k]=factor*np.sin(np.pi*k/(j-i))*(1+random.normalvariate(0.,.2))
         amplitudes[i+k]=factor*np.sin(np.pi*k/(j-i))*(1+random.normalvariate(0.,.1))
     return frequencias,amplitudes
@@ -136,6 +135,13 @@ def senito(ti,tf,media,amplitud,alphai,alphaf,factor,frequencias, amplitudes):
 # Deficion de parametros
 # ----------------------
 
+# Nombre archivo de parámetros específicos del ave
+# ------------------------------------------------
+
+ave_fname = 'AB010-bi.py'
+
+
+
 # Parametros especificos del modelo
 # ---------------------------------
 
@@ -143,104 +149,42 @@ gamma = 12300 #12500
 
 # Parametros tracto vocal
 uoch = 40*2700*2700 # 40*2700*2700
-uolg =  30./1. # 1./1.
+uolg =  1./1. # 1./1.
 rdis = 5000/1.0 # 5000/1.0
 
 v = np.zeros(5)
 v[0], v[1], v[2], v[3], v[4] =0.01,0.001,0.001, 0.0001, 0.0001
 
 L =  0.036 # No se que es !!!
-#L =  0.025
+# L =  0.025
 
 
-# Parametros especificos del canto
-# --------------------------------
 
-dt = 1/44100.0
-tiempo_total = 2.07
+# Cargo los parametros especificos del canto
+# ------------------------------------------
 
-# Inicializo las frecuencias fundamentales y amplitudes
-frequencias = np.zeros(np.int(tiempo_total/(dt)))
-amplitudes = np.zeros(np.int(tiempo_total/(dt)))
+# Cargo datos básicos del canto y genero trazas de frec fundamental
 
-# Inicializo los parametros de control
-alpha = np.zeros(np.int(tiempo_total/(dt)))
-beta = np.zeros(np.int(tiempo_total/(dt)))
-
-# Modifico valores variables que cree arriba
-for i in range(np.int(tiempo_total/(dt))):
-    alpha[i] = 0.15 # sistema no fona en este valor
-    beta[i] = 0.15 # ¿sistema no fona en este valor?
+with open(ave_fname) as f:
+    code = compile(f.read(), ave_fname, 'exec')
+    exec(code)
 
 
-# Para cada silaba defino: alpha, frecuencias fundamentales y amplitudes
-# expo(ti,tf,wi,wf,factor,frequencias,amplitudes)
-# rectas(ti,tf,wi,wf,factor,frequencias,amplitudes)
-# senito(ti,tf,media,amplitud,alphai,alphaf,factor,frequencias, amplitudes)
-
-# Notas intro
-expo(0.021233,0.070797,869.3,475.2,0.2239/0.944,frequencias, amplitudes)
-expo(0.163317,0.205335,825.5,453,0.2714/0.944,frequencias, amplitudes)
-expo(0.271632,0.313879,847.4,475.1,0.3216/0.944,frequencias, amplitudes)
-expo(0.383282,0.425770,891,475.1,0.3449/0.944,frequencias, amplitudes)
-rectas(0.467733,0.479150,812.64,812.64,0.08789/0.944,frequencias, amplitudes)
-expo(0.479152,0.527799,897,518.9,0.325/0.944,frequencias,amplitudes)
-rectas(0.592287,0.631223,573,431,0.3074/0.944,frequencias, amplitudes)
-rectas(0.699323,0.726651,847.4,497,0.2258/0.944,frequencias, amplitudes)
-
-rectas(0.794424,0.824770,1550,1550,0.08599/0.944,frequencias, amplitudes)
-# C
-expo(0.824771,0.929312,900,518.9,0.7771/0.944,frequencias, amplitudes)
-
-# B
-rectas(0.956319,0.969585,475,781,0.3498/0.944,frequencias, amplitudes)
-rectas(0.969586,1.000902,871,871,0.4434/0.944,frequencias, amplitudes)
-
-# A
-#rectas(1.035779,1.036574,475,570,0.389/0.944,frequencias, amplitudes)
-#rectas(1.036578,1.099138,579.72,559.72,0.9205/0.944,frequencias, amplitudes)
-#rectas(1.099139,1.107158,579.72,409.4,0.2376/0.944,frequencias, amplitudes)
-rectas(1.035681,1.108332,576,576,0.9205/0.944,frequencias, amplitudes)
 
 
-rectas(1.134231,1.139772,935,1548,0.1221/0.944,frequencias, amplitudes)
-rectas(1.141474,1.160209,546.62,546.62,0.1221/0.944,frequencias, amplitudes)
-rectas(1.160210,1.173081,546.62,344,0.4057/0.944,frequencias, amplitudes)
 
-senito(1.212321,1.223899,1132,1877,-np.pi/2,np.pi/2,0.2076/0.944,frequencias, amplitudes)
-senito(1.2240,1.251077,2534,963,-3*np.pi/2,15*np.pi/2,0.3622/0.944,frequencias, amplitudes)
-expo(1.259591,1.284083,1263,803,0.3622/0.944,frequencias, amplitudes)
+# ---------------
+# Calculamos Beta
+# ---------------
 
-rectas(1.357033,1.364902,1571,1571,0.531/0.944,frequencias, amplitudes)
-rectas(1.366208,1.382211,1592,1592,0.8903/0.944,frequencias,amplitudes)
-rectas(1.386501,1.486461,1023,475,0.7487/0.944,frequencias,amplitudes)
-rectas(1.521486,1.529366,497,847,0.3285/0.944,frequencias,amplitudes)
-rectas(1.530969,1.564968,977,828,0.4556/0.944,frequencias,amplitudes)
-
-# A2
-#rectas(1.599836,1.606783,497,565,0.3814/0.944,frequencias,amplitudes)
-#rectas(1.606784,1.660270,558,558,0.7228/0.944,frequencias,amplitudes)
-#rectas(1.660270,1.670171,558,387.5,0.4041/0.944,frequencias,amplitudes)
-rectas(1.599836,1.670171,564,564.5,0.7228/0.944,frequencias,amplitudes)
-
-rectas(1.699486,1.726439,540.73,540.73,0.4691/0.944,frequencias,amplitudes)
-rectas(1.726440,1.737927,540.73,365.6,0.2306/0.944,frequencias,amplitudes)
-senito(1.775943,1.787108,1142,563,-np.pi,np.pi,0.1919/0.944,frequencias, amplitudes)
-senito(1.787778,1.817603,2414,927,-np.pi/2,15*np.pi/2,0.2852/0.944,frequencias, amplitudes)
-expo(1.824992,1.848924,737,387,0.1677/0.944,frequencias,amplitudes)
-rectas(1.931650,1.948097,1550,1550,0.944/0.944,frequencias,amplitudes)
-expo(1.948099,2.06,1023,431,0.7555/0.944,frequencias,amplitudes)
-
-
-# Abro el archivo b_w que no se que es aun.
+# Abro el archivo b_w (re)
 bes,was = np.loadtxt('b_w_12300.txt',unpack=True)
 print('bes:', bes[0])
 print('was:',was[0])
 
 # Ajustamos was vs bes a un polinomio de grado 5
 # z es una lista de los coeficientes del polinomio ajustado
-#z = np.polyfit(was, bes, 5)
-z = np.polyfit(was, bes, 5)    
+z = np.polyfit(was, bes, 5)
 
 # Generamos objeto p, que es un polinomio de grado 5 (es z, hecho objeto)
 p = np.poly1d(z)
@@ -254,18 +198,16 @@ for i in range(np.int(tiempo_total/(dt))):
         beta[i] = p(frequencias[i])
  
     
- 
 
 # -----------
 # Integracion
 # -----------
 
-# Una integracion A PARTIR DE ACA ME CUESTA ENTENDER EL CODIGO
 
 n = 5 # TAMANO DEL SISTEMA DE ECUACIONES
 sonido = []
 
-# Variables de interes de la integracion que quizas quiera guardar
+# Variables intermedias  de la integracion que quizas quiera guardar
 x_out = []
 y_out = []
 #tiempo1 = []
@@ -281,7 +223,7 @@ fil1 = np.zeros(N)
 back1 = np.zeros(N)
 # feedback1 = 0
 
-
+# Integro
 for i in range(np.int(tiempo_total/(dt))):
     
     # Parametros dependientes del tiempo del sistema de ecuaciones
@@ -332,12 +274,14 @@ sonido = np.asarray(sonido)
 
 # Este paso es necesario para que el archivo wav se guarde correctamente
 # Ver la documentacion de: scipy.io.wavfile.write
-scaled = np.int16(sonido/np.max(np.abs(sonido)) * 32767)
+#scaled = np.int16(sonido/np.max(np.abs(sonido)) * 32767)
+scaled = np.int16(sonido/np.max(np.abs(sonido)) * 15000)
+
 write('sintesis_finch_uolg_30.wav', 44100, scaled)
 
 # Guardo salida de fuente.
 y_scaled = np.int16(y_out/np.max(np.abs(y_out)) * 32767)
-write('y.wav', 44100, y_scaled)
+write('y_alpha.wav', 44100, y_scaled)
 
 
 
