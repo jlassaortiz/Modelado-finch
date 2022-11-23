99d058f7f3ba32951333e9240133e103d120c585
Jav <javi.lassa@gmail.com>
2022-11-23 16:39:07 -0300

    add beak to white noise

--- white_noise_OEC.py
+++ white_noise_OEC.py
@@ -29,6 +29,8 @@ import numpy as np
 import matplotlib.pyplot as plt
      
 from scipy.io.wavfile import write
+from scipy.signal import resample
+
 global alp
 global b
 global feedback1 
@@ -49,10 +51,20 @@ np.seterr(over='raise')
 def ecuaciones(v, dv):
     i1,i2 = v
     dv[0]= i2
-    dv[1]= - 1/(Ch*Lg)*i1 - (Rd/Lg)*i2 + (1/Lg)*destimulodt
-
+    dv[1]= - 1/(Ch*Lg)*i1 - (Rh/Lg)*i2 + (1/Lg)*destimulodt
+    
     return dv
 
+def ecuaciones_beak(v, dv):
+    
+    # Params PRE Sanz Perl 2011
+
+    i1, i2, i3 = v
+    dv[0] = i2
+    dv[1] = - i1/(Lg*Ch) - Rh*i2*(1/Lb + 1/Lg) + i3*( 1/(Lg*Ch) - (Rb*Rh)/(Lb*Lg) ) + destimulodt/Lg + estimulo*Rh / (Lg*Lb)
+    dv[2] = - Lg*i2/Lb - Rb*i3 / Lb + estimulo/Lb
+  
+    return dv
 
 # Integrador RK4
 def rk4(dv,v,n,t,dt): #  dv es la funcion ecuaciones()
@@ -156,36 +168,42 @@ random.seed(1992)
 # 
 # -----------------------------------------------------------------------
 
-version = 'white_noise'
+version = 'white_noise_beak-A'
 guardar = False # guardo entrada y salida del filtro
 
 # Frecuencia y ventana temporal
 # -----------------------------
 
-sampling_freq = 44100 * 3 # Hz
+sampling_freq = 44100 # Hz
 dt = 1/sampling_freq
 print(f'\nsampling freq: {sampling_freq}')
 
-tiempo_total = 2.0
+tiempo_total = 5.0
 
 # Parametros del modelo
 # ---------------------
 
 # Parametros tracto vocal (filtro)
-Ch = calc_c(11000) # / 1.0 # Capacitancia Helmholtz
-Rd = 1500 # Resistencia disipación 
-Lg = 1.0 # Inductancia glotis
-L    = 0.036 / 1.0 # Longitud tubo (en metros) (0.036)
-coef_reflexion = -0.35 # -0.35 
-print(f'\nC: {Ch} \nR: {Rd} \nLg: {Lg} \n \nlargo_traquea: {L} \ncoef. reflexión: {coef_reflexion}')
+Ch = (1.5 * 1e-8) / 350 # calc_c(2000)*(1.0 **2) # / 1.0 # Capacitancia Helmholtz
+Lg = 20 # 1.0 # Inductancia glotis Helmholtz
+Rh = 1000 # 5000 # Resistencia disipación Helmholtz
+
+L  = np.round(0.036 * 1.0 , 3) # Longitud tubo (en metros) (0.036)
+coef_reflexion = -0.95 # -0.35 
+print(f'\nC: {Ch:.2e}  \nR: {Rh} \nLg: {Lg} \n \nlargo_traquea: {L} \ncoef. reflexión: {coef_reflexion}')
+
+# Coef sans perl 2011
+Lb = 1000
+Rb = 100000
+    
 
 # Condiciones iniciales
-v = np.zeros(2)
-v[0], v[1] =  0.001, 0.0001
+v = np.zeros(3)
+v[0], v[1], v[2] =  0.001, 0.0001, 0.0001
 
 
 # Tamaño del sistema de ecuaciones
-n = 2
+n = 3
 
 
 # -----------
@@ -202,7 +220,7 @@ noise_out = [] # salida filtro
 # elbeta1 = []
 
 # Retro-propagación
-N = int((L /(350*dt))//1) # 350 velocidad sonido en el aire
+N = int((L /(350*dt) ) // 1) # 350 velocidad sonido en el aire
 fil1 = np.zeros(N)
 back1 = np.zeros(N)
 # feedback1 = 0
@@ -211,19 +229,20 @@ back1 = np.zeros(N)
 # Integro
 # -------
 
-for i in range(np.int(tiempo_total/(dt))):
+for i in range(np.int(tiempo_total*sampling_freq)):
+    
+    noise = np.random.normal(-1.0, 1.0)
     
     # Parametros dependientes del tiempo del sistema de ecuaciones
     destimulodt = (fil1[N-1]-fil1[N-2])/dt
-    noise = np.random.normal(-1.0, 1.0)
-    
+    estimulo=fil1[N-1]
+
     # Integracion
     t =i*dt
     v.astype('float64')
-    rk4(ecuaciones,v,n,t,dt) # modifica v
+    rk4(ecuaciones_beak,v,n,t,dt) # modifica v
      
     # Actualizo las siguientes variables (retro-propagación)
-    estimulo=fil1[N-1]
     fil1[0]= noise + back1[N-1]
     back1[0]= coef_reflexion * fil1[N-1] 
     fil1[1:]=fil1[:-1]
@@ -232,10 +251,15 @@ for i in range(np.int(tiempo_total/(dt))):
 
     # Guardo salida del modelo (falta escalearla con la envolvente)
     noise_in.append(noise)
-    noise_out.append(v[1])
+    noise_out.append(v[2])
   
     
   
+# noise_in_resample =  resample(noise_in, int(tiempo_total*44100))
+# noise_out_resample = resample(noise_out, int(tiempo_total*44100))
+# sampling_freq = 44100
+# noise_in = noise_in_resample
+# noise_out = noise_out_resample
 
 # ----------------------
 # Guardo canto sintetico
@@ -246,9 +270,12 @@ for i in range(np.int(tiempo_total/(dt))):
 out_scaled = np.int16(noise_out/np.max(np.abs(noise_out)) * 32767)
 in_scaled = np.int16(noise_in/np.max(np.abs(noise_in)) * 32767)
 
+out_scaled = np.int16(noise_out/np.max(np.abs(noise_out)) * 32767)
+in_scaled = np.int16(noise_in/np.max(np.abs(noise_in)) * 32767)
+
 if guardar:
-    write(f'OUT_{version}_C_{Ch}_R_{Rd}_Lg_{Lg}_Ltraquea_{L}_coefReflex_{coef_reflexion}.wav', int(sampling_freq), out_scaled)
-    write(f'IN_{version}_C_{Ch}_R_{Rd}_Lg_{Lg}_Ltraquea_{L}_coefReflex_{coef_reflexion}.wav', int(sampling_freq), in_scaled)
+    write(f'OUT_{version}_C_{Ch:.2e}_R_{Rh}_Lg_{Lg}_Ltraquea_{L}_coefReflex_{coef_reflexion}.wav', int(sampling_freq), out_scaled)
+    write(f'IN_{version}_C_{Ch:.2e}_R_{Rh}_Lg_{Lg}_Ltraquea_{L}_coefReflex_{coef_reflexion}.wav', int(sampling_freq), in_scaled)
 
 
 
@@ -276,6 +303,7 @@ freq_out_smooth = sliding_window(freq_out, ws, step)
 # Ploteos
 # -------
 
+# SONIDO
 fig, axs = plt.subplots(2, sharex=True, figsize=(12,9))
 
 i = 0
@@ -286,29 +314,28 @@ i = i+1
 axs[i].plot(noise_out)
 axs[i].legend(['v[3]'], loc = 'lower left')
 
-fig.suptitle(f'IN-OUT_C_{Ch}_R_{Rd}_Lg_{Lg}_Ltraquea_{L}_coefReflex_{coef_reflexion}')
+fig.suptitle(f'IN-OUT_C_{Ch:.2e}_R_{Rh}_Lg_{Lg}_Ltraquea_{L}_coefReflex_{coef_reflexion}')
 plt.show()
 
 
 
-
+# FFT
 plt.figure(figsize=([16, 4]))
 plt.plot(freq_out, fft_out)
 plt.plot(freq_in, fft_in, ':r', alpha=0.4)
 plt.legend(['out FFT','in FFT'])
 plt.xlim([0,20000])
 plt.xlabel('Frecuencias (Hz)')
-
-fig.suptitle(f'FFT-IN-OUT_C_{Ch}_R_{Rd}_Lg_{Lg}_Ltraquea_{L}_coefReflex_{coef_reflexion}')
-
+plt.title(f'FFT-IN-OUT_C_{Ch:.2e}_R_{Rh}_Lg_{Lg}_Ltraquea_{L}_coefReflex_{coef_reflexion}')
 
 
+
+# FFT SUAVIZADO
 plt.figure(figsize=([16, 4]))
 plt.plot(freq_out_smooth, fft_out_smooth)
-plt.plot(freq_in_smooth, fft_in_smooth, 'r', alpha=0.4)
+plt.plot(freq_in_smooth,fft_in_smooth, 'r', alpha=0.4)
 plt.legend(['out FFT_smooth','in FFT_smooth'])
 plt.xlim([0,20000])
 plt.xlabel('Frecuencias (Hz)')
-
-fig.suptitle(f'FFT_SMOOTH-IN-OUT_C_{Ch}_R_{Rd}_Lg_{Lg}_Ltraquea_{L}_coefReflex_{coef_reflexion}')
-
+plt.ylabel('power')
+plt.title(f'FFT_SMOOTH-IN-OUT_C_{Ch:.2e}_R_{Rh}_Lg_{Lg}_Ltraquea_{L}_coefReflex_{coef_reflexion}')
