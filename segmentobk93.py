8004ee3cb63b047de79d1ba35ce1918425ea9510
Jav <javi.lassa@gmail.com>
2022-11-23 16:37:45 -0300

    Create segmentobk93.py

---
+++ segmentobk93.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 17 12:09:19 2022
+
+@author: javierlassaortiz
+"""
+
+
+
+nombre_ave = 'segmentobk93'
+nombre_BOS = 'segmentobk93.wav'
+
+# Calculamos trazas de frecuencias fundamentales de este canto en particular
+# --------------------------------------------------------------------------
+
+# Para cada silaba defino: alpha, frecuencias fundamentales
+
+# expo(ti,tf,
+# wi,wf,
+# frequencias, 
+# silabas_timestamp)
+
+# rectas(ti,tf,
+# wi,wf,
+# frequencias,
+# silabas_timestamp)
+
+# senito(ti,tf,
+# media,amplitud,
+# alphai,alphaf,
+# frequencias,
+# silabas_timestamp)
+
+
+rectas(0.290,0.345,
+1139,1139,
+frequencias,
+silabas_timestamp)
